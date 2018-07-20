a06a15d93a4feb38935ab5410b10574ff4f10f5b
Marc Mascort Bou <marc@articstudio.com>
2018-07-20 13:09:59 +0200

    Json Output

---
+++ src/Outputs/Json.php
@@ -0,0 +1,19 @@
+<?php
+namespace BeyondCode\QueryDetector\Outputs;
+
+use Illuminate\Support\Collection;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
+
+class Json implements Output
+{
+
+    public function output(Collection $detectedQueries, Response $response)
+    {
+        if ($response instanceof JsonResponse) {
+            $data = $response->getData(true);
+            $data['warning_queries'] = $detectedQueries;
+            $response->setData($data);
+        }
+    }
+}
