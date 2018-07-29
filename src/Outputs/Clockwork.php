cbf5174a79c4b1da875ffc599c46d1dc7f850fac
Stanislav Plakhin <plakhin@users.noreply.github.com>
2018-07-30 00:00:33 +0600

    Add Clockwork output
    Simply adds warnings to Clockwork Log if N+1 queries detected

---
+++ src/Outputs/Clockwork.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BeyondCode\QueryDetector\Outputs;
+
+use Illuminate\Support\Collection;
+use Symfony\Component\HttpFoundation\Response;
+
+class Clockwork implements Output
+{
+    public function output(Collection $detectedQueries, Response $response)
+    {
+        clock()->warning("{$detectedQueries->count()} N+1 queries detected:", $detectedQueries->toArray());
+    }
+}
